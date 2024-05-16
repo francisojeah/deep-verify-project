e477c4d842c5360d058039a4e54d60a080bdcddc
francisojeah <ojeahfrancis@gmail.com>
2024-05-16 06:29:01 +0100

    setup mongoose, winston, error filter and interceptor

--- web-apps/deep-verify-backend/src/app.module.ts
+++ web-apps/deep-verify-backend/src/app.module.ts
@@ -1,10 +1,68 @@
-import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import { ScheduleModule } from '@nestjs/schedule';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import {
+  utilities as nestWinstonModuleUtilities,
+  WinstonModule,
+} from 'nest-winston';
+import * as winston from 'winston';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
+import { ErrorFilter } from './filters/error.filter';
+import { Logger, MiddlewareConsumer, Module } from '@nestjs/common';
+import { ErrorInterceptor } from './interceptors/error.interceptor';
+import { RequestLoggerMiddleware } from '@utils/request-logger.middleware';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ScheduleModule.forRoot(),
+    // ServeStaticModule.forRoot({
+    //   rootPath: join(__dirname, '../../..', 'coupe-frontend', 'dist'),
+    //   exclude: ['/api*'],
+    // }),
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRootAsync({
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get('DB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+    WinstonModule.forRoot({
+      transports: [
+        new winston.transports.Console({
+          format: winston.format.combine(
+            winston.format.timestamp(),
+            winston.format.ms(),
+            nestWinstonModuleUtilities.format.nestLike('MyApp', {
+              colors: true,
+              prettyPrint: true,
+            }),
+          ),
+        }),
+      ],
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    Logger,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: ErrorInterceptor,
+    },
+    {
+      provide: APP_FILTER,
+      useClass: ErrorFilter,
+    },
+  ],
 })
-export class AppModule {}
+export class AppModule {
+  constructor() {}
+
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(RequestLoggerMiddleware).forRoutes('*');
+  }
+}
