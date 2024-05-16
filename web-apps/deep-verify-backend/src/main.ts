7f9aced54f85faccd4c47629a25316e26d8e329a
francisojeah <ojeahfrancis@gmail.com>
2024-05-16 06:30:36 +0100

    setup winston, swaggerui, api versioning and validation

--- web-apps/deep-verify-backend/src/main.ts
+++ web-apps/deep-verify-backend/src/main.ts
@@ -1,8 +1,73 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
+import { WinstonModule } from 'nest-winston';
+import { transports, format } from 'winston';
+import 'winston-daily-rotate-file';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    logger: WinstonModule.createLogger({
+      transports: [
+        new transports.DailyRotateFile({
+          filename: `logs/%DATE%-error.log`,
+          level: 'error',
+          format: format.combine(format.timestamp(), format.json()),
+          datePattern: 'YYYY-MM-DD',
+          zippedArchive: false,
+          maxFiles: '30d',
+        }),
+        new transports.DailyRotateFile({
+          filename: `logs/%DATE%-combined.log`,
+          format: format.combine(format.timestamp(), format.json()),
+          datePattern: 'YYYY-MM-DD',
+          zippedArchive: false,
+          maxFiles: '30d',
+        }),
+        new transports.Console({
+          format: format.combine(
+            format.cli(),
+            format.splat(),
+            format.timestamp(),
+            format.printf((info) => {
+              return `${info.timestamp} ${info.level}: ${info.message}`;
+            }),
+          ),
+        }),
+      ],
+    }),
+  });
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle(process.env.APP_NAME)
+    .setDescription(process.env.APP_DESCRIPTION)
+    .setVersion(process.env.API_VERSION)
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig, {
+    ignoreGlobalPrefix: false,
+  });
+
+  // app.enableCors({ origin: [allowed] });
+  app.enableCors();
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  // app.setGlobalPrefix('/backend');
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+
+  try {
+    SwaggerModule.setup('/api', app, document, {});
+  } catch (error) {
+    this.logger.error(error);
+  }
+
+  await app.listen(process.env.APP_SERVER_LISTEN_PORT, '0.0.0.0');
+
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
