32c1f1a9bfab0328261c131994cab6ed003dd161
francisojeah <ojeahfrancis@gmail.com>
2024-07-12 01:17:48 +0100

    updated the design for the not found page

--- web-apps/deep-verify-frontend/src/pages/Errors/NotFound.tsx
+++ web-apps/deep-verify-frontend/src/pages/Errors/NotFound.tsx
@@ -16,7 +16,7 @@ const NotFound = () => {
   return (
     <div className="flex h-screen p-4 justify-center items-center">
       <div className="w-full md:w-1/2 mx-auto lg:p-16 lg:mt-10 flex flex-col gap-8 justify-center items-center">
-        <p className="lg:mt-8 lg:text-2xl text-secondary font-bold text-center">
+        <p className="lg:mt-8 lg:text-2xl text-black dark:text-white text-secondary font-bold text-center">
           This page does not exist
         </p>
 
@@ -28,13 +28,13 @@ const NotFound = () => {
 
         <div className="w-full flex justify-between gap-5 mt-4">
           <button
-            className="w-1/2 rounded-xl min-h-[48px] text-white bg-custom-primary-1 text-base font-medium hover:bg-transparent hover:text-custom-primary-1 border-2 border-custom-primary-1 focus:outline-none"
+            className="w-1/2 rounded-xl min-h-[48px] text-white bg-custom-primary text-base font-medium hover:bg-transparent hover:text-custom-primary border-2 border-custom-primary focus:outline-none"
             onClick={handleGoBack}
           >
             Go back
           </button>
           <button
-            className="w-1/2 rounded-xl bg-white min-h-[48px] text-custom-primary-1 text-base font-medium hover:bg-custom-primary-1 hover:text-white  border-2 border-custom-primary-1 focus:outline-none"
+            className="w-1/2 rounded-xl bg-transparent min-h-[48px] text-custom-primary text-base font-medium hover:bg-custom-primary hover:text-white  border-2 border-custom-primary focus:outline-none"
             onClick={onClickGoHome}
           >
             Go home
