ab763581bd2d5747aa26909612ce1b27945f4e69
francisojeah <ojeahfrancis@gmail.com>
2024-07-12 01:16:28 +0100

    updated the tailwind config themes

--- web-apps/deep-verify-frontend/tailwind.config.js
+++ web-apps/deep-verify-frontend/tailwind.config.js
@@ -1,12 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require("tailwindcss/defaultTheme");
+const flowbite = require("flowbite-react/tailwind");
 
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "../../node_modules/flowbite-react/**/*.{js,ts,jsx,tsx}",
+    flowbite.content(),
   ],
+  darkMode: 'class',
   theme: {
     extend: {
       fontFamily: {
@@ -17,24 +19,51 @@ export default {
       },
       colors: {
         custom: {
+          primary:"#8b5cf6",
           success: "#8ADD21",
           danger: "#DD2121",
-          primary: {
-            1: "#1179FF",
-            2: "#134493",
-            3: "#ACBCCA",
-          },
-          secondary: {
-            1: "#B5D2CB",
-            2: "#CDE1DC",
-            3: "#E6F0ED",
-          },
-          accent: {
-            1: "#40E0BA",
-            2: "#7FEAD1",
-            3: "#BFF4E8",
-          },
         },
+        'medium-purple': {
+          '50': '#f7f3ff',
+          '100': '#efe9fe',
+          '200': '#e2d6fe',
+          '300': '#cbb5fd',
+          '400': '#ad8bfa',
+          '500': '#8b5cf6',
+          '600': '#713aed',
+          '700': '#5e28d9',
+          '800': '#4e21b6',
+          '900': '#421d95',
+          '950': '#2a1065',
+      },
+        light: {
+          background: "#FFFFFF", 
+          primaryText: "#060606",
+          secondaryText: "#6C757D", 
+          primaryButtonBg: "#8b5cf6", 
+          primaryButtonText: "#FFFFFF",
+          secondaryButtonBg: "#6C757D", 
+          secondaryButtonText: "#FFFFFF", 
+          success: "#28A745", 
+          warning: "#FFC107", 
+          link: "#17A2B8", 
+          border: "#E0E0E0", 
+          card: "#FFFFFF", 
+        },
+        dark: {
+          background: "#060606", 
+          primaryText: "#FFFFFF", 
+          secondaryText: "#B0B3B8", 
+          primaryButtonBg: "#8b5cf6", 
+          primaryButtonText: "#FFFFFF", 
+          secondaryButtonBg: "#495057", 
+          secondaryButtonText: "#FFFFFF", 
+          success: "#81C784", 
+          warning: "#FFA000", 
+          link: "#00B8D4", 
+          border: "#424242", 
+          card: "#1E1E1E", 
+        }
       },
     },
     screens: {
@@ -42,8 +71,6 @@ export default {
     },
   },
   plugins: [
-    require("flowbite/plugin")({
-      charts: true,
-    }),
+    flowbite.plugin(),
   ],
 };
