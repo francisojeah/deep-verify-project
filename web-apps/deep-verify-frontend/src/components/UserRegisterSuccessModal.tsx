dbb6ae9249e68ffa51e8d1f33208774b2a62789f
francisojeah <ojeahfrancis@gmail.com>
2024-07-12 01:31:03 +0100

    created modal for user registration

---
+++ web-apps/deep-verify-frontend/src/components/UserRegisterSuccessModal.tsx
@@ -0,0 +1,43 @@
+import { Modal } from "flowbite-react";
+
+interface UserRegisterSuccessModalProps {
+  openModal: boolean;
+  setOpenModal: any;
+  email: string;
+}
+
+const UserRegisterSuccessModal = ({
+  openModal,
+  setOpenModal,
+  email,
+}: UserRegisterSuccessModalProps) => {
+  return (
+    <>
+      <Modal
+        size={"lg"}
+        dismissible
+        show={openModal}
+        onClose={setOpenModal}
+      >
+        <Modal.Header className="border-none pb-0"></Modal.Header>
+        <Modal.Body className="pt-0">
+          <div className="flex gap-2 items-center">
+            <img src="/assets/icons/user-register-success-icon.svg" />
+            <div className="flex flex-col gap-4">
+              <p className="font-bold text-xl text-custom-primary-1">
+                Verify Your Account
+              </p>
+              <p className="text-base text-black">
+                We’ve sent an email to{" "}
+                <span className="font-bold text-base">{email}</span>. Follow the
+                steps provided in the email to verify your account.
+              </p>
+            </div>
+          </div>
+        </Modal.Body>
+      </Modal>
+    </>
+  );
+};
+
+export default UserRegisterSuccessModal;
