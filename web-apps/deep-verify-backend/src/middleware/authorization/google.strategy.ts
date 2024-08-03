b720a7396b27122aba53983c5793f59aeab90bc7
francisojeah <ojeahfrancis@gmail.com>
2024-08-03 07:32:52 +0100

    updated the google strategy

--- web-apps/deep-verify-backend/src/middleware/authorization/google.strategy.ts
+++ web-apps/deep-verify-backend/src/middleware/authorization/google.strategy.ts
@@ -8,7 +8,7 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
     super({
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: 'https://localhost:3000/google/redirect',
+      callbackURL: 'https://deep-verify-backend.onrender.com/google/redirect',
       scope: ['email', 'profile'],
     });
   }
